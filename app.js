5767b76f378827b459411f461aca409974856f66
Chinczykk <gamon.lama@gmail.com>
2018-03-18 11:17:58 +0100

    Allow players to move

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app)
+require('./variables.js');
 
-app.use('/', express.static(__dirname + '/client'));
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);
 console.log('Server started');
 
+var SOCKET_LIST = {}
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
-    console.log("socket.connection")
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 495;
+    SOCKET_LIST[socket.id] = socket
+    console.log("socket connected")
+
+    socket.on('disconnect', function() {
+        delete SOCKET_LIST[socket.id]
+        console.log("socket disconnected");
+    })  
+
+    socket.on('move', function(data) {
+        if(data.directory === 'left') {
+            socket.x -= SPEED;
+        } else if(data.directory === 'right') {
+            socket.x += SPEED;
+        }
+    })
 })
+
+setInterval(function() {
+    var pack = [];
+    for(var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i]
+        pack.push({
+            x: socket.x,
+            y: socket.y
+        })
+    }
+    for(var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i]
+        socket.emit('newPosition', pack)
+    }
+}, 1000/25)
---
+++ variables.js
@@ -0,0 +1,1 @@
+global.SPEED = 10;
